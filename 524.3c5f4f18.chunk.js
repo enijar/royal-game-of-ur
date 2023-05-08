e3787d30edfbc7520210e454c4381b7d03489fcb
enijar <TheEnijar@gmail.com>
2023-05-08 10:05:55 +0000

    Deploying to gh-pages from @ enijar/royal-game-of-ur@7b7da141597286ebc17e4e1c0b668c8d42bde11b 🚀

---
+++ 524.3c5f4f18.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[524],{524:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var l=t(294),u=t(655);function r(){return l.createElement("main",null,l.createElement("h1",null,"Home"),l.createElement(u.rU,{to:"/play"},"Play Now!"))}}}]);
+//# sourceMappingURL=524.3c5f4f18.chunk.js.map
